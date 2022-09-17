500fe51c6487a9151a0e101d049ffee0c5257c93
Backhaus <44726485+matbks@users.noreply.github.com>
2022-09-17 19:20:38 -0300

    main

--- index.js
+++ index.js
@@ -15,19 +15,34 @@ const app = express();
 // Permite ler json
 app.use(express.json());
 
+//
+app.engine('html', require('ejs').renderFile); //instalar biblioteca ejs
+
 // Permite ler o body
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Exibe página de login
 app.use(express.static(path.join(__dirname, 'login')));
+app.use(express.static(path.join(__dirname, 'main')));
 
 // Define como reagir à uma request de POST
 app.post('/', (request, response) => {
 
     // Responde à request com um "ok"
-    response.send("ok");
-
+    // response.sendFile(path.join(__dirname, 'login'))
+    // response.send("post")
+    // response.sendFile(__dirname + '/cube/index.html');
+    response.sendFile(__dirname + '/main/index.html');
+    
 });
+
+app.get('/main', (request, response) => {
+
+    response.sendFile(__dirname + '/main/index.html');
+    // response.send("get")
+    console.log("get")
+
+})
 
 // Fica sempre atento à requests na porta 5000
 app.listen(port, () => {
